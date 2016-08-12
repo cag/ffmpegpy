49b11ae86f2b3b462a6d168df3aa2733183beb5c
Alan Lu <gotoalanlu@gmail.com>
2016-08-11 20:37:10 -0500

    Made a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ffmpegpy',
+      version='0.1',
+      description='Another FFmpeg wrapper.',
+      url='http://github.com/cag/ffmpegpy',
+      author='Alan Lu',
+      author_email='gotoalanlu@gmail.com',
+      license='MIT',
+      packages=['ffmpegpy'],
+      zip_safe=False)
